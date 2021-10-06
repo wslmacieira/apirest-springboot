bfabc7ac8515d280c2cf2f3ebb9509dd643a5758
wslmacieira <wslmacieira@outlook.com>
2021-10-06 12:18:50 -0300

    Configurando banco de dados Postgres e criando Model

---
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -0,0 +1,59 @@
+package com.produtos.apirest.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto implements Serializable{
+    private static final Long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal quantidade;
+
+    private BigDecimal valor;
+    
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValor() {
+        return this.valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+    
+}
